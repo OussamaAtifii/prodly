26250d93b664514d0e348dc2e593c4292d0ffde9
OussamaAtifii <oussamaati03@gmail.com>
2025-03-29 15:54:32 +0100

    feat(tasks): change task middleware to check project membership

--- backend/src/middlewares/taskMiddleware.ts
+++ backend/src/middlewares/taskMiddleware.ts
@@ -1,3 +1,4 @@
+import MemberService from '@services/memberService';
 import ProjectService from '@services/projectService';
 import TaskService from '@services/taskService';
 import { NextFunction, Request, Response } from 'express';
@@ -8,23 +9,24 @@ export default async function taskMiddleware(
   next: NextFunction
 ) {
   const id = Number(req.params.id);
-  const userId = req.session.user?.id;
+  const userId = Number(req.session.user?.id);
 
   try {
     if (!id) {
       return res.status(400).json({ message: 'Invalid task id' });
     }
 
-    const task = await TaskService.getTaskById(Number(id));
+    const task = await TaskService.getTaskById(id);
 
     if (!task) {
       return res.status(404).json({ message: 'Task not found' });
     }
 
     const project = await ProjectService.getById(task.projectId);
+    const isMember = await MemberService.getMember(userId, project.id);
 
     // Check if the session user is the owner of the project
-    if (project.userId !== Number(userId)) {
+    if (project.userId !== userId && !isMember) {
       return res.status(403).json({
         message: 'You do not have permission to perform this action',
       });
