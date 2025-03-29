62f301eea691ce69a5a8bf36a6f7eaba27e369af
OussamaAtifii <oussamaati03@gmail.com>
2025-03-29 15:25:06 +0100

    feat(config): add email configuration variables

--- backend/src/config/config.ts
+++ backend/src/config/config.ts
@@ -6,3 +6,8 @@ dotenv.config();
 export const PORT = process.env.PORT || 3000;
 export const JWT_SECRET_KEY =
   process.env.JWT_SECRET_KEY || 'default-secret-key';
+
+export const EMAIL_PASS = process.env.EMAIL_PASS || '';
+
+export const CLIENT_URL = process.env.CLIENT_URL;
+export const EMAIL_USER = process.env.EMAIL_USER;
