be9f61ca1388e45f1b1ff90431bbd3fd051f32e9
OussamaAtifii <oussamaati03@gmail.com>
2025-04-01 21:03:09 +0200

    feat(members): add invite member dialog

---
+++ frontend/src/app/features/invitation/dialogs/invite-member-dialog/invite-member-dialog.component.ts
@@ -0,0 +1,59 @@
+import { Component, inject, signal } from '@angular/core';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import { Invitation } from '@features/invitation/models/invitation';
+import { InvitationService } from '@features/invitation/services/invitation.service';
+import { ProjectService } from '@features/projects/services/project.service';
+import { PrintErrorComponent } from '@shared/components/print-error/print-error.component';
+
+@Component({
+  selector: 'app-invite-member-dialog',
+  imports: [
+    MatDialogModule,
+    MatButtonModule,
+    ReactiveFormsModule,
+    PrintErrorComponent,
+  ],
+  templateUrl: './invite-member-dialog.component.html',
+  styleUrl: './invite-member-dialog.component.css',
+})
+export class InviteMemberDialogComponent {
+  private projectService = inject(ProjectService);
+  private invitationService = inject(InvitationService);
+
+  private readonly dialogRef = inject(
+    MatDialogRef<InviteMemberDialogComponent>,
+  );
+
+  error = signal('');
+
+  invitationForm = new FormGroup({
+    email: new FormControl<string>('', [Validators.required, Validators.email]),
+  });
+
+  onSubmit() {
+    if (this.invitationForm.invalid) return;
+    const projectId = this.projectService.project()?.id as number;
+    const email = this.invitationForm.get('email')?.value as string;
+
+    const data: Invitation = {
+      projectId,
+      email,
+    };
+
+    this.invitationService.sendInvitation(data).subscribe({
+      next: (value) => {
+        this.dialogRef.close();
+      },
+      error: (error) => {
+        this.error.set(error.error.message);
+      },
+    });
+  }
+}
